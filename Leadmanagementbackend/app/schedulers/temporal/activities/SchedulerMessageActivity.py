a0298243a857d6f4c3ee36698a341a9b0895d79e
jitin <jitin3401@gmail.com>
2026-09-23 23:48:05 +0530

    scheduled message corrected

--- Leadmanagementbackend/app/schedulers/temporal/activities/SchedulerMessageActivity.py
+++ Leadmanagementbackend/app/schedulers/temporal/activities/SchedulerMessageActivity.py
@@ -4,6 +4,7 @@ from temporalio import activity
 
 from app.db.session import get_db
 from app.dependencies.ObjectConstructor import build_channel_service
+from app.enums.message import ScheduledMessageStatus
 from app.models.scheduled_message import ScheduledMessage
 from app.repositories.ChannelConnectionRepository import ChannelConnectionRepository
 from app.repositories.Scheduled_message_repository import ScheduledMessageRepository
@@ -25,6 +26,14 @@ async def send_scheduled_message(
 
 
             result=await channel_service.send_message(lead_id=scheduled_message.lead_id,connection_id=scheduled_message.channel_connection_id,content=scheduled_message.content,channel_id=connection.channel_id,reply_to_message_id=None)
+            if result:
+                scheduled_message.status=ScheduledMessageStatus.SENT
+                db.add(scheduled_message)
+                db.commit()
+
+
+
+
 
 
 
