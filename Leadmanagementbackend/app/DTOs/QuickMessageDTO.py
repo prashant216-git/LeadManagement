f6cd6b898a83c1f044caaa66fb0d027f5a1ded54
prashant <saxena21636@gmail.com>
2026-09-24 17:00:08 +0530

    added gcp bucket config

--- Leadmanagementbackend/app/DTOs/QuickMessageDTO.py
+++ Leadmanagementbackend/app/DTOs/QuickMessageDTO.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from uuid import UUID
 
 from pydantic import BaseModel, ConfigDict
@@ -6,18 +5,18 @@ from pydantic import BaseModel, ConfigDict
 from enums.message import QuickMessageType
 
 
-class QuickMessageAttachmentDTO:
+class QuickMessageResponse(BaseModel):
+    id: UUID
+
+    message_text: str | None = None
+    title: str | None = None
+    attachment_type: QuickMessageType
+    is_active: bool
+
+    attachment_url: str | None = None
+
+    model_config = ConfigDict(from_attributes=True)
 
 
-
-    class Response(BaseModel):
-        id: UUID
-
-        message_text: str | None = None
-        title: str | None = None
-        attachment_url: str | None = None
-        attachment_type: QuickMessageType
-        is_active: bool
-
-
-        model_config = ConfigDict(from_attributes=True)
+class ListResponse(BaseModel):
+    items: list[QuickMessageResponse]
